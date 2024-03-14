309851ad2b42a11bae2a89063750b6dd9cea7d4e
Patrick Hauck <patrickhauck0@gmail.com>
2024-03-14 14:25:33 -0300

    Criação do CRUD utilizando Express e Postman para testar requisição.

--- server.js
+++ server.js
@@ -1,18 +1,20 @@
-import http from "http";
+//import http from "http";
+import app from "./src/app.js";
 
 const PORT = 3000;
 
-const rotas = {
+/* const rotas = {
     "/": "Curso de Node.js",
     "/livros": "Entrei na rota livros",
     "/autores": "entrei na rota autores"
-};
+}; */
 
-const server = http.createServer((req, res) => {
+/* const server = http.createServer((req, res) => {
     res.writeHead(200, { "Content-Type": "text/plain" });
     res.end(rotas[req.url]);
-});
+}); */
 
-server.listen(PORT, () => {
+//Trocou de server para app, pois criamos um arquivo para manager de rotas
+app.listen(PORT, () => {
     console.log("servidor escutando!");
 });
