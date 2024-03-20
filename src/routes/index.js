773700fe842997bdeb80ea4ac0066048b15e6949
Patrick Hauck <patrickhauck0@gmail.com>
2024-03-20 13:19:19 -0300

    FInalização da API.

--- src/routes/index.js
+++ src/routes/index.js
@@ -3,7 +3,7 @@ import livros from "./livrosRoutes.js";
 import autores from "./autoresRoutes.js"
 
 const routes = (app) => {
-    app.route("/").get((req, res) => res.status(200).send("Curso de Node.js"));
+    app.route("/").get((req, res) => res.status(200).send("Conectado."));
 
     app.use(express.json(), livros, autores);
 };
